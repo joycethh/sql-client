c40d2b93f2ffab9b19e883a7c8815456e8e29a4c
joycethh <joycethh@outlook.com>
2022-12-02 11:36:35 -0600

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import Login from "../src/pages/login/Login";
 import Register from "./pages/register/Register";
 
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
 function App() {
   return (
     <div>
-      <Login />
-      <Register />
+      <RouterProvider router={router} />
     </div>
   );
 }
