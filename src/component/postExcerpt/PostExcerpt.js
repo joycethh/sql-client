dc2e75844c40c020b028bae3b94504bf845df137
joycethh <joycethh@outlook.com>
2022-12-05 16:04:54 -0600

    add comment component

--- src/component/postExcerpt/PostExcerpt.js
+++ src/component/postExcerpt/PostExcerpt.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import Comments from "../comments/Comments";
 import "./postExcerpt.scss";
 import {
   MoreHoriz,
@@ -8,6 +9,8 @@ import {
 } from "@mui/icons-material";
 
 const PostExcerpt = ({ post }) => {
+  const [openComments, setOpenComments] = useState(false);
+
   return (
     <div className="postExcerpt">
       <div className="container">
@@ -16,28 +19,29 @@ const PostExcerpt = ({ post }) => {
             <img src={post.profilePic} alt="" />
             <div className="details">
               <span className="authorName">{post.name}</span>
-              <span className="date">time</span>
+              <span className="date">1 min ago</span>
             </div>
           </div>
           <MoreHoriz />
         </div>
-
         <div className="content">
           <p>{post.desc}</p>
           <img src={post.img} alt="" />
         </div>
-
-        <div className="reactions">
+        <div className="interations">
           <div className="item">
             <ThumbUpOutlined />
           </div>
-          <div className="item">
+          <div className="item" onClick={() => setOpenComments(!openComments)}>
             <TextsmsOutlined />
+            12 Comments
           </div>
           <div className="item">
             <ShareOutlined />
+            Share
           </div>
         </div>
+        {openComments && <Comments />}
       </div>
     </div>
   );
