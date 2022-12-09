18f676ef3d1cd970354cfa0659d846dab0783849
joycethh <joycethh@outlook.com>
2022-12-09 15:53:35 -0600

    test out register user works

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,19 +1,61 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./register.scss";
+import axios from "axios";
 
 const Register = () => {
+  const [input, setInput] = useState({
+    username: "",
+    email: "",
+    password: "",
+    repeatedPassword: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInput({ ...input, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await axios.post(
+      "http://localhost:8000/api/auth/register",
+      input
+    );
+    console.log("response", response);
+  };
   return (
     <div className="register">
       <div className="card">
         <div className="right">
           <h1>Register</h1>
-          <form>
-            <input type="text" placeholder="Username" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <input type="password" placeholder="Repeat Password" />
-            <button>Submit</button>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+              onChange={handleChange}
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              onChange={handleChange}
+            />
+            <button onClick={handleSubmit}>Submit</button>
           </form>
         </div>
         <div className="left">
