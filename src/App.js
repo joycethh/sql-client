e8499e154b188aa5f8d39650526b07abeb13e33b
joycethh <joycethh@outlook.com>
2023-04-21 10:18:59 -0500

    remove console

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useContext } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
-import { useAuthContext } from "./context/authContext";
 import Routers from "./router/Routers";
 
 function App() {
   const queryClient = new QueryClient();
   const { isDarkMode } = useContext(DarkModeContext);
-  const { currentUser } = useAuthContext();
-  console.log("currentUser in app", currentUser);
 
   return (
     <>
