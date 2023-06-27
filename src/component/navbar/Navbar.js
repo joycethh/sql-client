a66fe9749a9e9f2a77d5241038c1098e8cd92d11
joycethh <joycethhh@gmail.com>
2023-06-27 15:56:39 -0500

    update the layout of the navbar to accomodate the postion of the search bar

--- src/component/navbar/Navbar.js
+++ src/component/navbar/Navbar.js
@@ -14,45 +14,52 @@ import { useAuthContext } from "../../context/authContext";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   const { isDarkMode, toggle } = useDarkModeContext();
   const { currentUser, logout } = useAuthContext();
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
+
   return (
-    <div className="navbar">
-      <div className="left">
-        <Link to="/" style={{ textDecoration: "none" }}>
-          <span>Funget2</span>
-        </Link>
-        <Search />
-      </div>
-
-      <div className="right">
-        {currentUser ? (
-          <div className="user" onClick={toggleMenu}>
-            <img src={currentUser?.profilePic} alt="" />
-            <span>{currentUser?.name}</span>
+    <>
+      <div className="navbar">
+        <div className="navbar-top">
+          <div className="left">
+            <Link to="/" style={{ textDecoration: "none" }}>
+              <span>Funget2</span>
+            </Link>
           </div>
-        ) : (
-          <PersonOutlineOutlined />
-        )}
-
-        {isMenuOpen && (
-          <div className="dropdown-menu">
-            <div className="speech-bubble"></div>
-            <div className="menu-item" onClick={() => logout()}>
-              <LogoutOutlined /> Logout
-            </div>
-            <div className="menu-item" onClick={toggle}>
-              {isDarkMode ? <LightModeOutlined /> : <BedtimeOutlined />} Dark
-              Mode
-            </div>
+          <div className="middle-bottom">
+            <Search />
           </div>
-        )}
+          <div className="right">
+            {currentUser ? (
+              <div className="user" onClick={toggleMenu}>
+                <img src={currentUser?.profilePic} alt="" />
+                <span>{currentUser?.name}</span>
+              </div>
+            ) : (
+              <PersonOutlineOutlined />
+            )}
+
+            {isMenuOpen && (
+              <div className="dropdown-menu">
+                <div className="speech-bubble"></div>
+                <div className="menu-item" onClick={() => logout()}>
+                  <LogoutOutlined /> Logout
+                </div>
+                <div className="menu-item" onClick={toggle}>
+                  {isDarkMode ? <LightModeOutlined /> : <BedtimeOutlined />}{" "}
+                  Dark Mode
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
